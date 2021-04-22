42234af5d26679f92994a7daeefeb12227419506
seanwb95 <69982131+seanwb95@users.noreply.github.com>
2021-04-23 01:31:30 +0200

    Add files via upload
    Made the tracer work, need to calc impact velocity and time spent

--- ECPC_prediction2.py
+++ ECPC_prediction2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import pandas as pd
 
 start = time.time()
 
@@ -20,7 +21,6 @@ def calc_E(phi_p, theta_p, F_pc, phiD, phiN):
     Beq = 31000e-9
     R_E = 6371e3
 
-
     L_R1 = np.arcsin(np.sqrt(F_pc/(2*np.pi*R_E**2*Beq)))
 
     v_R1 = (1/(2*np.pi*R_E*Beq*np.sin(2*L_R1)))*(phiD-phiN)
@@ -33,7 +33,7 @@ def calc_E(phi_p, theta_p, F_pc, phiD, phiN):
 
     lD = 2*thtD*R_E*np.sin(L_R1)
     lN = 2*thtN*R_E*np.sin(L_R1)
-
+    
     E_D = E_B + phiD/lD
     E_N = E_B - phiN/lN
 
@@ -48,6 +48,7 @@ def calc_E(phi_p, theta_p, F_pc, phiD, phiN):
     colattitutde (lambda_R1) for all theta values. This
     is done with the parameters L_R1, E_B, E_N and E_D.
     """
+    
     for i in range(len(theta)):
         if theta[i] < thtN:
             Vpcb[i] = -R_E*np.sin(L_R1)*(E_N*theta[i])
@@ -83,12 +84,14 @@ def calc_E(phi_p, theta_p, F_pc, phiD, phiN):
     if the position is in the polar cap region. Here we make the substitution
     of Lambda_x = ln(tan(L_x/2)).
     """
-
-    L_p = np.deg2rad(90-phi_p)
+    
+    L_p = (np.pi/2)-phi_p
     dL = np.pi/18
     L_R2 = L_R1 + dL
+    
+    Lambda_p = 0
 
-    if np.tan(phi_p/2)==0:
+    if np.tan(L_p/2)==0:
         Lambda_p = -np.inf
     else:
         Lambda_p = np.log(np.tan(L_p/2))
@@ -100,6 +103,8 @@ def calc_E(phi_p, theta_p, F_pc, phiD, phiN):
     dphi_T = 0
     dphi_L = 0
     
+    
+    #Endre if statemenst kan løse ting
     for m in np.arange(1, 21, 1):
         if Lambda_p <= Lambda_R2 and Lambda_p > Lambda_R1:
             dphi_L += Sm[m]*m*np.sin(m*theta_p)* (np.cosh(m*(Lambda_p-Lambda_R2))/np.sinh(m*(Lambda_R1-Lambda_R2)))
@@ -107,36 +112,30 @@ def calc_E(phi_p, theta_p, F_pc, phiD, phiN):
         elif Lambda_p <= Lambda_R1:
             dphi_L += Sm[m]*m*np.sin(m*theta_p)*np.exp(m*(Lambda_p-Lambda_R1))
             dphi_T += Sm[m]*m*np.cos(m*theta_p)*np.exp(m*(Lambda_p-Lambda_R1))
-
     
+#Sjekk ut sin conditions
+    E_lambda = -1/(R_E*np.sin(np.pi/2-L_p)) * dphi_L
+    E_theta = -1/(R_E*np.sin(np.pi/2-L_p)) * dphi_T
 
-    E_lambda = -1/(R_E*np.sin(L_p)) * dphi_L
-    E_theta = -1/(R_E*np.sin(L_p)) * dphi_T
 
-
-    return L_R1, v_R1, Vpcb, Sm, E_lambda, E_theta
-
-a = 60
-b = 90 #the reason to why there is a 0-value at the first point. 
-       #Increase to 90 and solve inf-problem
+    return L_R1, E_lambda, E_theta#,L_p
 
 n = 30
-ph_p = np.linspace(b,a, n) #latitude phi
+
+ph_p = np.deg2rad(np.linspace(90,60, n)) #latitude phi
 t_p = np.linspace(2*np.pi, 0, n) #longitude theta
 
 E_lam = np.zeros([len(ph_p),len(t_p)])
 E_tht = np.zeros([len(ph_p),len(t_p)])
 
 
-for p in range(len(ph_p)):
-    print("step %d of %g" %(p, n))
-    for t in range(len(t_p)):
-        L, v, Vp, Sm, E_L, E_T = calc_E(ph_p[p],t_p[t], 0.4e9, 0, 50e3)
-        E_lam[p][t] = E_L
-        E_tht[p][t] = E_T
+#for p in range(len(ph_p)):
+ #   print("step %d of %g" %(p, n))
+ #   for t in range(len(t_p)):
+ #       L, E_L, E_T = calc_E(ph_p[p],t_p[t], 0.4e9, 0, 50e3)
+ #       E_lam[p][t] = E_L
+ #       E_tht[p][t] = E_T
 
-        
-MLT = np.linspace(0, 24, len(Vp))
 
 bobx = np.zeros((n, n))
 boby = np.zeros_like(bobx)
@@ -145,7 +144,7 @@ theta = np.zeros_like(bobx)
 
 for i in range(len(bobx)):
     for j in range(len(bobx)):
-        r[i, j] = 90 - ph_p[i]
+        r[i, j] = (np.pi/2) - ph_p[i]
         theta[i, j] = t_p[j]
         boby[i, j] = r[i, j]*np.cos(theta[i, j]) 
         bobx[i, j] = r[i, j]*np.sin(theta[i, j])
@@ -154,7 +153,7 @@ for i in range(len(bobx)):
 colors = E_lam
 plt.figure()
 ax = plt.subplot(polar = "true")
-plt.pcolormesh(theta, r,colors ,cmap = "jet")
+plt.pcolormesh(theta, np.rad2deg(r),colors ,cmap = "jet")
 plt.gca().invert_yaxis()
 
 ax.set_theta_offset(np.deg2rad(90))
@@ -176,95 +175,51 @@ a function that calculates V for the latitude and longtidue for a single positio
 It will have the arguments phi_p.
 """
 
-"""
-OBS, maa oppdateres ved generell bruk
-"""
 
-
-def calc_V(FPC, RCR_D, RCR_N):
+def calc_Bp(tr_pos):
     Beq = 31000e-9
-    
+    tr_Bp = 2*Beq*np.cos(tr_pos)
+    return tr_Bp
 
-    N = 30
-    A = 60
-    B = 90
-    
-    V_ph_p = np.linspace(B, A, N) #latitude phi
-    V_t_p = np.linspace(2*np.pi, 0, N) #longitude theta
-    V_L_p = np.zeros(N) 
-    Bp = np.zeros((N, N))
-    
-    #VLp går fra 1-30, men skal det være 60-90?
-    for ii in range(N):
-        V_L_p[ii] = np.deg2rad(90-V_ph_p[ii])
-        for jj in range(N):
-            Bp[ii][jj] = 2*Beq*np.cos(V_L_p[ii])
-    
+def calc_V(V_ph_p,V_t_p,FPC, RCR_D, RCR_N):
+    Beq = 31000e-9
+    Bp= 2*Beq*np.cos(V_ph_p)
 
-    E_lam_v = np.zeros([len(V_ph_p),len(V_t_p)])
-    E_tht_v = np.zeros([len(V_ph_p),len(V_t_p)])
-    
-    V_tht = np.zeros([len(V_ph_p),len(V_t_p)])
-    V_lam = np.zeros([len(V_ph_p),len(V_t_p)])
-    
-    
-    for p in range(len(V_ph_p)):
-        for t in range(len(V_t_p)):
-            L, v, Vp, Sm, E_L, E_T = calc_E(V_ph_p[p],V_t_p[t], FPC, RCR_D, RCR_N)
-            E_lam_v[p][t] = E_L
-            E_tht_v[p][t] = E_T
-            V_tht[p][t] = E_lam_v[p][t]/Bp[p][t]
-            V_lam[p][t] = E_tht_v[p][t]/Bp[p][t]
-    return V_tht, V_lam, Bp
-
-#dmlt = 1.125 #how big the patch is in mlt range
+    L, E_L, E_T = calc_E(V_ph_p,V_t_p, FPC, RCR_D, RCR_N)
+    V_tht = E_L/Bp
+    V_lam = E_T/Bp
+    return V_tht, V_lam
 
 """
 Creating tracers, here sMLT decides starting position in MLT. 
 """
 
-def trco(s_MLT, Rr, x):
+def trco(s_MLT, Rr):
     """ converts tracer coordinates in MLT and colatitude into xy-coordinates """
     """ converts between mlt and radians. x = mlt """
     start_theta = s_MLT*2*np.pi/24 #rad
-    start_x=Rr*np.cos(start_theta)
-    start_y=Rr*np.sin(start_theta)
+    start_x=np.rad2deg(Rr)*np.cos(start_theta)
+    start_y=np.rad2deg(Rr)*np.sin(start_theta)
     return start_x, start_y
 
-def create_tracer(sMLT, L_p, dmlt):
+def create_tracer(sMLT, dmlt, tr_nr):
+    
+    pc_flux = 0.3e9
+    Beq = 31000e-9
+    R_E = 6371e3
+
+    Rr1 = np.arcsin(np.sqrt(pc_flux/(2*np.pi*R_E**2*Beq)))
+    Rr = np.linspace(Rr1, Rr1, tr_nr)
     
     tracer_xy = [] #empty list to append tracer position
-    tracer_mlt1 = 12-sMLT
-    tracer_mlt2= 12-sMLT+dmlt
-    l1 = 7 # number of tracer particles 
+    tracer_mlt1 = sMLT-12
+    tracer_mlt2= sMLT+dmlt-12
     
-    mlts = np.linspace(tracer_mlt1,tracer_mlt2,l1)
-    for i in np.arange(0,l1):
-        tracer_xy.append(trco(mlts[i], L_p, sMLT))   
+    mlts = np.linspace(tracer_mlt1,tracer_mlt2,tr_nr)
+    
+    for i in np.arange(0, tr_nr):
+        tracer_xy.append(trco(mlts[i], Rr[i]))
     return tracer_xy
-
-def tracer_path(V_theta, V_lambda,sMLT, L_P, dmlt, FPC, RCR_D, RCR_N):
-    RR = np.zeros((n, n))
-    TT = np.zeros((n, n))
-    Vx = np.zeros((n, n))
-    Vy = np.zeros((n, n))
-    
-    tracers_pos = create_tracer(sMLT, L_P, dmlt)
-    VT, VL, BP = calc_V(FPC, RCR_D, RCR_N)
-    
-    for c in range(n): 
-        for cc in range(n):
-            Vx[c, cc] = VL[c]*np.cos*(theta[cc]) - r[c]*VT[cc]*np.sin(theta[cc])
-            Vy[c, cc] = VL[c]*np.sin*(theta[cc]) + r[c]*VT[cc]*np.cos(theta[cc])
-
-#    for i in range():
-#        #find V at tracer location
-#        tr_x[i] = tr_x[i-1] + vx*dt
-#        tr_y[i] = tr_y[i-1] + vy*dt
-#        
-#        RR = np.sqrt(x[i]**2+y[i]**2)
-#        TT = np.arctan2(y[i],x[i])      
-    return Vx, Vy
 
 
 """
@@ -328,84 +283,161 @@ plt.legend(["synth_Fpc"])
 plt.show()
 
 
-V_theta = np.zeros(len(Nn))
-V_lambda = np.zeros(len(Nn))
 
-for e in range(len(Nn)):   
-    , Vl, Bp = calc_V(synth_Fpc[e], synth_phi_D[e], synth_phi_N[e])
-#    V_theta[e] = Vt
-#    V_lambda[e] = Vl
+"""
+Creating starting positions for the tracers
+"""
+
+tr_nr = 13 #number of tracers
+sMLT = 10.0
+dmlt = 23
+
+tracers_spos = np.deg2rad(create_tracer(sMLT,dmlt, tr_nr))
+tracer_pos = []
+    
+start_theta = np.zeros(len(tracers_spos))
+start_phi = np.zeros(len(tracers_spos))
+
+"""
+Convert to polar coordinates
+"""
+
+
+for j in range(len(tracers_spos)):
+    start_theta[j]= np.sqrt(tracers_spos[j][0]**2+tracers_spos[j][1]**2)
+    start_phi[j] = np.arctan2(tracers_spos[j][1],tracers_spos[j][0])
+    tracer_pos.append([start_theta[j], start_phi[j]])
+
+tracer_rt = tracer_pos.copy()
+
+R_E = 6371e3
+
+
+B_tracer = np.zeros(len(tracer_pos))
+tracer_r = np.zeros([len(Nn),len(tracer_rt)])
+tracer_t = np.zeros([len(Nn),len(tracer_rt)])
+tracer_Vr = np.zeros([len(Nn),len(tracer_rt)])
+tracer_Vt = np.zeros([len(Nn),len(tracer_rt)])
+tracer_EL = np.zeros([len(Nn),len(tracer_rt)])
+tracer_ET = np.zeros([len(Nn),len(tracer_rt)])
+
+has_collided = 0
+
+             
+
+  #  print("Positions:", tracer_rt)
+ #   print("____________________________________")
+ #   print("Velocities:", tracer_vel)
+ #   print("1:",tracer_rt[1])
+ #   print("0:",tracer_rt[0])
 
 sE_lam = np.zeros([len(Nn), len(ph_p),len(t_p)])
 sE_tht = np.zeros([len(Nn),len(ph_p),len(t_p)])
-ocb_location = np.zeros([len(Nn),len(ph_p),len(t_p)])
+fart_t = np.zeros([len(Nn),len(ph_p),len(t_p)])
+fart_l = np.zeros([len(Nn),len(ph_p),len(t_p)])
 
 for tt in range(len(Nn)):
+#for tt in range(23,len(Nn)):
     print("step %d of %g" %(tt, len(Nn)))
     for pp in range(len(ph_p)):
         for qq in range(len(t_p)):
-            sL, sv, sVp, sSm, sE_L, sE_T = calc_E(ph_p[pp],t_p[qq], synth_Fpc[tt], synth_phi_D[tt], synth_phi_N[tt])
-            ocb_location[tt][pp][qq] = sL
-            sE_lam[tt][pp][qq] = sE_L
-            sE_tht[tt][pp][qq] = sE_T
-        
-    colorss = sE_tht[tt]
-    plt.figure()
-    ax1 = plt.subplot(polar = "true")
-    plt.pcolormesh(theta, r,colorss,cmap = "jet")
-    plt.gca().invert_yaxis()
+            #sL, sE_L, sE_T = calc_E(ph_p[pp],t_p[qq], synth_Fpc[tt], synth_phi_D[tt], synth_phi_N[tt])
+            #sE_lam[tt][pp][qq] = sE_L
+            #sE_tht[tt][pp][qq] = sE_T
+            Vt, Vl =  calc_V(ph_p[pp],t_p[qq], synth_Fpc[tt], synth_phi_D[tt], synth_phi_N[tt])
+            fart_t[tt][pp][qq] = Vt
+            fart_l[tt][pp][qq] = Vl
+    THETA, R = np.meshgrid(t_p, ph_p)
+            
+            
+#     dr = fart_l[tt]
+#     dt = fart_t[tt]
     
-    ax1.set_theta_offset(np.deg2rad(90))
-    ax1.set_xticks((0,np.pi/2,np.pi,3*np.pi/2))
-    ax1.set_xticklabels(['12','18','00','06'])
-    ax1.set_yticks(np.arange(10,31,10))
-    ax1.set_yticklabels(['60','70','80']) 
+#     f = plt.figure()
+#     ax3 = f.add_subplot(111, polar=True)
+#     ax3.quiver(THETA, R, dr*np.cos(THETA)-dt*np.sin(THETA), dr*np.sin(THETA)+dt * np.cos(THETA))
+#     ax3.set_rlim(np.deg2rad(60), np.deg2rad(90))
+#     plt.show()
+      
+    #colorss = sE_tht[tt]
+    # plt.figure()
+    # ax1 = plt.subplot(polar = "true")
+    # plt.pcolormesh(theta, np.rad2deg(r),colorss,cmap = "jet")
+    # plt.gca().invert_yaxis()
     
-    cbar1 = plt.colorbar()
-    cbar1.set_label('$E_\theta (mV \: m^{-1})$', rotation=270)
+    # ax1.set_theta_offset(np.deg2rad(90))
+    # #ax1.set_rlim(np.deg2rad(60), np.deg2rad(90))
+    # ax1.set_xticks((0,np.pi/2,np.pi,3*np.pi/2))
+    # ax1.set_xticklabels(['12','18','00','06'])
+    # ax1.set_yticks(np.arange(10,31,10))
+    # ax1.set_yticklabels(['70','80','90']) 
     
-    plt.clim(-0.1,0.1)
-    abc = "%d" %(tt)
-    cb = Nn[tt] * 60
-    cba = "%d" %(cb)
-    plt.title("Eletric field component in $\theta$ direction t = " + cba + "s")
-    plt.savefig("%d synth_ET.jpg" %(tt))
-    plt.show()
+    # cbar1 = plt.colorbar()
+    # cbar1.set_label('$E_\\theta (mV \: m^{-1})$', rotation=270)
+    
+    # plt.clim(-0.035,0.035)
+    # abc = "%d" %(tt)
+    # cb = Nn[tt] * 60
+    # cba = "%d" %(cb)
+    # plt.title("Eletric field component in $\\theta$ direction t = " + cba + "s")
+    # plt.savefig("%d synth_ET.jpg" %(tt))
+    # plt.show()
+   
+
+for i in range(len(Nn)):
+    print("Tracer positions")
+    print("step %d of %g" %(i, len(Nn)))
+    for p in range(len(tracer_pos)):
+            B_tracer = 0               
+            L, E_L, E_T = calc_E((np.pi/2)-tracer_rt[p][0],tracer_rt[p][1], synth_Fpc[i], synth_phi_D[i], synth_phi_N[i])
+            B_tracer = calc_Bp(tracer_rt[p][0])
+            
+            VT_p = E_L/(B_tracer)
+            VL_p = E_T/(B_tracer)
+            tracer_rt[p] = [tracer_rt[p][0]+VT_p*(dt/(R_E)),tracer_rt[p][1]+VL_p*(dt/((R_E*np.sin(tracer_rt[p][0]))))]
+            
+
+            tracer_EL[i][p] = E_L
+            tracer_ET[i][p] = E_T
+            tracer_Vr[i][p] = VL_p  
+            tracer_Vt[i][p] = VT_p
+            tracer_r[i][p] = tracer_rt[p][0]
+            tracer_t[i][p] = tracer_rt[p][1]
+
+          
+    # plt.figure()
+    # ax = plt.subplot(polar = "true")
+    # ax.set_theta_offset(np.deg2rad(90))
+    # ax.set_rlim(np.deg2rad(0), np.deg2rad(30))
+    # ax.set_xticks((0,np.pi/2,np.pi,3*np.pi/2))
+    # ax.set_xticklabels(['12','18','00','06'])
+    # ax.plot(tracer_t[i], tracer_r[i], "o")
+    # plt.title("%d" %(i))
+    # plt.show()  
 
 
-    colorsss = sE_lam[tt]
-    plt.figure()
-    ax2 = plt.subplot(polar = "true")
-    plt.pcolormesh(theta, r,colorsss,cmap = "jet")
-    plt.gca().invert_yaxis()
+    # colorsss = sE_lam[tt]
+    # plt.figure()
+    # ax2 = plt.subplot(polar = "true")
+    # plt.pcolormesh(theta, np.rad2deg(r),colorsss,cmap = "jet")
+    # plt.gca().invert_yaxis()
     
-    ax2.set_theta_offset(np.deg2rad(90))
-    ax2.set_xticks((0,np.pi/2,np.pi,3*np.pi/2))
-    ax2.set_xticklabels(['12','18','00','06'])
-    ax2.set_yticks(np.arange(10,31,10))
-    ax2.set_yticklabels(['60','70','80']) 
+    # ax2.set_theta_offset(np.deg2rad(90))
+    # ax2.set_xticks((0,np.pi/2,np.pi,3*np.pi/2))
+    # ax2.set_xticklabels(['12','18','00','06'])
+    # ax2.set_yticks(np.arange(10,31,10))
+    # ax2.set_yticklabels(['70','80','90']) 
     
-    cbar2 = plt.colorbar()
-    cbar2.set_label('$E_\lambda (mV \: m^{-1})$', rotation=270)
+    # cbar2 = plt.colorbar()
+    # cbar2.set_label('$E_\lambda (mV \: m^{-1})$', rotation=270)
 
-    plt.clim(-0.1,0.1)
-    plt.title("Eletric field component in $\lambda$ direction t = " + cba + "s")
-    plt.savefig("Lambda %d synth.jpg" %(tt))
-    plt.show()
+    # plt.clim(-0.05,0.05)
+    # plt.title("Eletric field component in $\lambda$ direction t = " + cba + "s")
+    # #plt.savefig("Lambda %d synth.jpg" %(tt))
+    
+    # plt.show()
  
 
 
 stop = time.time()
 print(stop - start, "s")
-
-LPP = np.zeros(len(ph_p))
-for z in range(len(ph_p)):
-    LPP[z]= np.deg2rad(90-ph_p[z])
-
-for ee in range(len(Nn)):    
-    VX, VY = tracer_path(V_theta[ee], V_lambda[ee],13.0, LPP[ee], 1.125, synth_Fpc[ee], synth_phi_D[ee], synth_phi_N[ee])
-
-#Lag func som oppdaterer
-# i funk, kall på make tracer
-#Make tracer er i kartesisk, konverter
-#prøv å plot func i polar koordinater
